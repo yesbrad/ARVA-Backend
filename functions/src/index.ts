1d9e07bf3a2bfb6c830bc8c522540900bcf665ec
brad <b.bradfrancis@gmail.com>
2021-03-03 12:52:49 +1100

    Added Send Grid

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,8 @@ import * as admin from 'firebase-admin';
 var serviceAccount = require("../arva-161d140f7f23.json");
 import * as functions from 'firebase-functions';
 var cors = require('cors')({ origin: true });
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail')
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
@@ -15,25 +17,31 @@ export * from './api/brochures';
 
 export const sendMail = functions.https.onRequest(async (request, response) => {
 	return cors(request, response, async () => {
-		// const { message } = JSON.parse(request.body);
+		const { message, email, subject, name } = JSON.parse(request.body);
 	
-		// if ( message == null) {
-		// 	console.log('Missing Email Data');
-		// 	response.status(500).send('Missing Email Data');
-		// }
-
-		try {
-			await admin.firestore().collection('mail').add({
-				to: 'b.bradfrancis@gmail.com',//'award32@bigpond.com',
-				message: {
-				  subject: 'Hello from Firebase!',
-				  html: 'This is an <code>HTML</code> email body.',
-				},
-			});
-			response.send('Finished Sending');
-		} catch {
-			console.log("Server Error Failed Email");
-			response.status(500).send('Server Error Failed Email');
+		if ( message == null || email == null || subject == null || name == null) {
+			console.log('Missing Email Data');
+			response.status(500).send('Missing Email Data');
 		}
+
+		sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+		console.log(process.env.SENDGRID_API_KEY)
+
+		const msg = {
+			to: 'b.bradfrancis@gmail.com', // Change to your recipient
+			from: email, // Change to your verified sender
+			subject,
+			text: `Message on Australian RV from ${name}: \n ${message}`,
+		}
+		
+		sgMail.send(msg).then(() => {
+			console.log('Email sent')
+			response.send("Email Sent");
+		}).catch((error: Error) => {
+			console.log(error.message);
+			console.log(error.name);
+			response.sendStatus(500);
+		})
+
 	})
 });
