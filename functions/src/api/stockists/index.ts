8fbd7ad97c456fa255a955474c954e67221f8b7a
brad <b.bradfrancis@gmail.com>
2021-03-04 12:06:29 +1100

    Cors Fix

--- functions/src/api/stockists/index.ts
+++ functions/src/api/stockists/index.ts
@@ -20,43 +20,35 @@ export const getStockistsAll = functions.https.onRequest(async (request, respons
 });
 
 export const addStockist = functions.https.onRequest(async (request, response) => {
-	response.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000');
-	response.setHeader('Access-Control-Allow-Credentials', 'true');
-
-	if (request.method === 'OPTIONS') {
-		response.set('Access-Control-Allow-Methods', 'POST');
-		response.set('Access-Control-Allow-Headers', 'Authorization');
-		response.set('Access-Control-Max-Age', '3600');
-		response.status(204).send('');
-	}
-
-	const { ID, name, website, address, state} = JSON.parse(request.body);
+	return cors(request, response, async () => {
+		const { ID, name, website, address, state } = JSON.parse(request.body);
 	
-	try {
-		await admin.auth().verifyIdToken(BearerToJWT(request.headers.authorization))
-	} catch {
-		response.status(401).send('Invalid Token');
-	}
+		try {
+			await admin.auth().verifyIdToken(BearerToJWT(request.headers.authorization))
+		} catch {
+			response.status(401).send('Invalid Token');
+		}
 	
-	if(ID == null || name == null || website == null || state == null) {
-		console.log('Missing Stockist Data');
-		response.status(500).send('Missing Stockist Data');
-	}
+		if (ID == null || name == null || website == null || state == null) {
+			console.log('Missing Stockist Data');
+			response.status(500).send('Missing Stockist Data');
+		}
 
-	try {
-		await admin.firestore().collection(stockistCollectionID).doc(String(ID)).set({
-			ID,
-			website,
-			name,
-			address,
-			state,
-		});
-		console.log('Finished Upload')
-		response.send('Finished Upload');
-	} catch {
-		console.log("Server Error Stockist adding to storage");
-		response.status(500).send('Server Error Stockist adding to storage');
-	}
+		try {
+			await admin.firestore().collection(stockistCollectionID).doc(String(ID)).set({
+				ID,
+				website,
+				name,
+				address,
+				state,
+			});
+			console.log('Finished Upload')
+			response.send('Finished Upload');
+		} catch {
+			console.log("Server Error Stockist adding to storage");
+			response.status(500).send('Server Error Stockist adding to storage');
+		}
+	});
 });
 
 export const removeStockist = functions.https.onRequest(async (request, response) => {
